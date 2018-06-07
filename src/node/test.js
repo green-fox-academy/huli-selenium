7ecda247fcbb1e3fa43fa1a78962bc79d21f84d6
Pontiac1-1 <suppanmiklos@gmail.com>
2018-06-07 17:57:08 +0200

    dotnet and node tests up

--- src/node/test.js
+++ src/node/test.js
@@ -6,13 +6,14 @@ const webdriver = require('selenium-webdriver'),
 
 const driver = new webdriver.Builder()
   .forBrowser('chrome')
+  .setChromeOptions(driver.Options().headless())
   .build();
 
-driver.get('http://localhost:3000');
+driver.get('http://google.com');
 driver.wait(check_title);
 function check_title() {
   let promise = driver.getTitle().then( function(title) {
-    if(title === 'HuliApp' || title === 'React App') {
+    if(title === 'Google') {
       console.log('success');
       return true;
     } else {
@@ -21,4 +22,3 @@ function check_title() {
   });
   return promise;
 };
-
